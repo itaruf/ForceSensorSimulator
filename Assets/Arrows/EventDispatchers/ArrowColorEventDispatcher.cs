1af259fbc97fad9d3d18a5fd8b71930d903fd8ac
itaruf <imane93100@hotmail.fr>
2024-05-05 02:10:25 +0200

    feat: add Arrows' color-related event dispatchers as scriptable objects
    Specialized and instantiable child of the "Color Event Dispatcher" asset.
    
    Creation of 2 instances to be triggered on color changes for the following situations:
    + "Arrow low magnitude color" changes
    + "Arrow high magnitude color" changes

---
+++ Assets/Arrows/EventDispatchers/ArrowColorEventDispatcher.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+// Arrow Event Dispatcher for Color changes
+[CreateAssetMenu(fileName = "Arrow Color Event Dispatcher", menuName = "Arrow Event Dispatcher/Arrow Color Event Dispatcher", order = 0)]
+public class ArrowColorEventDispatcher : ColorEventDispatcher
+{
+}
