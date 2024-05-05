140bc86bb6dbec5511529a5113e8e4574fed8c02
itaruf <imane93100@hotmail.fr>
2024-05-06 00:08:11 +0200

    refactor: modify the order for some Event Dispatchers
    Change from  1 to 0.

--- Assets/Arrows/EventDispatchers/ArrowVisibilityEventDispatcher.cs
+++ Assets/Arrows/EventDispatchers/ArrowVisibilityEventDispatcher.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 // Scriptable object that serves as an event dispatcher to handle and dispatch Arrow-related (bool) messages
-[CreateAssetMenu(fileName = "Arrow Visibility Event Dispatcher", menuName = "Arrow Event Dispatcher/Arrow Visibility Event Dispatcher", order = 1)]
+[CreateAssetMenu(fileName = "Arrow Visibility Event Dispatcher", menuName = "Arrow Event Dispatcher/Arrow Visibility Event Dispatcher", order = 0)]
 public class ArrowVisibilityEventDispatcher : BoolEventDispatcher
 {
 }
