81b2e40854f34bd9fc40ad5af44c339405c87b1e
itaruf <imane93100@hotmail.fr>
2024-05-05 02:40:45 +0200

    feat: add a manager that handles the arrows' visual settings uniformly
    Keeps track and updates data for all in-game arrows when receiving changes through events.
    Attributes include:
    + Colors for low and high magnitude values from the force readings.
    + Arrows' visibility state in-game.

---
+++ Assets/Arrows/Managers/ArrowForceVisualizerManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+// Class that manages the settings for the arrows representing the force sensors readings
+public class ArrowForceVisualizerManager : MonoBehaviour
+{
+    public static ArrowForceVisualizerManager instance;
+
+    // Colors for low and high magnitude values
+    [SerializeField] private Color _colorLowMagnitude;
+    [SerializeField] private Color _colorHighMagnitude;
+
+    // Keep track of the visibility of all generated arrows
+    private bool _arrowVisibility = true;
+
+    // Arrow Event Dispatchers to bind to
+    public ArrowColorEventDispatcher eDI_ArrowLowMagnitudeColor;
+    public ArrowColorEventDispatcher eDI_ArrowHighMagnitudeColor;
+    public ArrowVisibilityEventDispatcher eDI_ArrowVisibility;
+
+    // Arrow Events to trigger in response to the event dispatchers
+    public static event ColorEvents.OnColorChange onArrowColorChangeByMagnitude;
+    public static event VisibilityEvents.OnVisibilityChange onArrowVisibilityChange;
+
+    // Property accessors
+    public Color ColorLowMagnitude { get => _colorLowMagnitude; }
+    public Color ColorHighMagnitude { get => _colorHighMagnitude; }
+    public bool ArrowVisibility { get => _arrowVisibility; }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        if (eDI_ArrowLowMagnitudeColor != null)
+            eDI_ArrowLowMagnitudeColor.AddListener(UpdateLowMagnitudeColor);
+
+        if (eDI_ArrowHighMagnitudeColor != null)
+            eDI_ArrowHighMagnitudeColor.AddListener(UpdateHighMagnitudeColor);
+
+        if (eDI_ArrowVisibility != null)
+            eDI_ArrowVisibility.AddListener(UpdateVisibility);
+    }
+
+    void UpdateVisibility(bool bVisibility)
+    {
+        _arrowVisibility = bVisibility;
+        onArrowVisibilityChange?.Invoke(bVisibility);
+    }
+
+    private void UpdateLowMagnitudeColor(Color color)
+    {
+        _colorLowMagnitude = color;
+        onArrowColorChangeByMagnitude?.Invoke(_colorLowMagnitude);
+    }
+
+    private void UpdateHighMagnitudeColor(Color color)
+    {
+        _colorHighMagnitude = color;
+        onArrowColorChangeByMagnitude?.Invoke(_colorHighMagnitude);
+    }
+}
