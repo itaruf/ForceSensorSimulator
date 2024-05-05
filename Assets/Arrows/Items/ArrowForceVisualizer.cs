dca9da3e64ccff7202dc8e8fc7ecd995dc4f14aa
itaruf <imane93100@hotmail.fr>
2024-05-05 02:55:18 +0200

    feat: arrow that visually represents the data from its associated force sensor
    Starts by associating with its parent force sensor to read the data from.
    
    Binds itself to the arrow manager's events to update the color/visibility state on changes.
    + The color transitions smoothly from one extreme to another.
    + Visibility = false stops the arrow's update, Visibility = true starts it.
    
    Draw a debug arrow every frame, where the:
    + position matches the one provided by lib.
    + direction corresponds to the force readings.
    + color is based on the magnitude of the force readings.
    + scale is based on the scaled magnitude and used as a scalar for the direction to get the length of the arrow.

---
+++ Assets/Arrows/Items/ArrowForceVisualizer.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using UnityEngine;
+
+// Class that visually represents the associated force sensor readings with an arrow
+public class ArrowForceVisualizer : MonoBehaviour
+{
+    // Associated force sensor
+    [SerializeField] private ForceSensor _forceSensor;
+
+    // Color changes based on the magnitude
+    private Color _color;
+
+    // Update the arrow with the simulated force data provided
+    private Coroutine _updateArrowWithForceDataCoroutine;
+
+    private void Start()
+    {
+        if (!_forceSensor)
+            _forceSensor = GetComponentInParent<ForceSensor>();
+
+        if (ArrowForceVisualizerManager.instance != null)
+        {
+            ArrowForceVisualizerManager.onArrowColorChangeByMagnitude += (color) => { _color = color; };
+            ArrowForceVisualizerManager.onArrowVisibilityChange += (bVisibility) =>
+            {
+                if (bVisibility)
+                {
+                    if (_updateArrowWithForceDataCoroutine == null)
+                    {
+                        _updateArrowWithForceDataCoroutine = StartCoroutine(UpdateArrowWithForceData());
+                    }
+                }
+                else
+                {
+                    if (_updateArrowWithForceDataCoroutine != null)
+                    {
+                        StopCoroutine(_updateArrowWithForceDataCoroutine);
+                        _updateArrowWithForceDataCoroutine = null;
+                    }
+                }
+            };
+
+            if (ArrowForceVisualizerManager.instance.ArrowVisibility)
+                _updateArrowWithForceDataCoroutine = StartCoroutine(UpdateArrowWithForceData());
+        }
+    }
+
+    private IEnumerator UpdateArrowWithForceData()
+    {
+        float lerpTime = 0;
+        float transitionDuration = /*1f*/ForceSensorManager.instance.updateDelay;
+        float transitionSpeed = 1f;
+
+        while (true)
+        {
+            if (!_forceSensor)
+                yield break;
+
+            // Get the provided force from the external library
+            Vector3 force = _forceSensor.ForceSensorData.force;
+
+            // Get the length of the force
+            float magnitude = force.magnitude;
+
+            // Get the direction of the force
+            Vector3 normalizedForce = force.normalized;
+
+            // The magnitude is capped
+            float magnitudeThreshold = 5f;
+            float scaledMagnitude = magnitude / magnitudeThreshold;
+
+            // Smoothly transitioning the color based on the magnitude
+            Color targetColor = Color.Lerp(ArrowForceVisualizerManager.instance.ColorLowMagnitude, ArrowForceVisualizerManager.instance.ColorHighMagnitude, scaledMagnitude);
+
+            lerpTime += Time.deltaTime / transitionDuration; 
+             
+            _color = Color.Lerp(_color, targetColor, lerpTime * transitionSpeed);
+
+            DebugArrow.DrawForDebug(transform.position, normalizedForce, _color, scaledMagnitude);
+
+            if (lerpTime >= 1f)
+                lerpTime = 0; // Reset lerp time
+
+            yield return null;
+        }
+    }
+}
