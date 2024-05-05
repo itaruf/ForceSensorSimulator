b0a5c01c39ee6149b8b0ea179c501926c29b17f0
itaruf <imane93100@hotmail.fr>
2024-05-05 02:16:35 +0200

    feat: add Arrows' visibility-related event dispatchers as scriptable objects
    Specialized and instantiable child of the "Visibility Event Dispatcher" asset.
    
    Creation of an instance to bind events/methods to and to trigger on arrows' visibility state changes (i.e. whether or not to show/hide arrows in-game).

---
+++ Assets/Arrows/EventDispatchers/ArrowVisibilityEventDispatcher.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+// Arrow Event Dispatcher for Visibility changes
+[CreateAssetMenu(fileName = "Arrow Visibility Event Dispatcher", menuName = "Arrow Event Dispatcher/Arrow Visibility Event Dispatcher", order = 1)]
+public class ArrowVisibilityEventDispatcher : VisiblityEventDispatcher
+{
+}
